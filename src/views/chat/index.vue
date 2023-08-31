ed505b99043b88141e6faec90feb0edf72a88fcd
onceisy <onceisy@gmail.com>
2023-08-31 11:11:41 +0800

    docs: site url

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -467,7 +467,7 @@ onUnmounted(() => {
               <div>永久免费提供学习和测试，支持上下文，支持开启关闭联网模式，支持保存会话，切勿发布至国内平台或微信分享</div>
               <div class="mt-1">
                 <n-button quaternary type="primary">
-                  <a target="_blank" href="http://ai.onceisy.com/">http://ai.onceisy.com</a>
+                  <a target="_blank" href="https://ai.onceisy.com/">https://ai.onceisy.com</a>
                 </n-button>
                 <span>国内服务器镜像（仅境内可访问）</span>
               </div>
